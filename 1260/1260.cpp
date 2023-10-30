c7e75b3e1315292734fa16011869094ba4088c6c
Jiwon Jung <jiwojung@c8r1s4.42seoul.kr>
2023-10-30 17:24:25 +0900

    save

--- 1260/1260.cpp
+++ 1260/1260.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <list>
+#define VISITED 3
+#define MAX_NODES 1000
 using namespace std;
 
-int graph[1000][1000];
+typedef struct s_node
+{
+	int	node_num;
+	struct s_node *next;
+}	t_node;
+
+int	visited[MAX_NODES];
 
 void	dfs(int start)
 {
@@ -11,16 +19,23 @@ void	dfs(int start)
 
 int	main()
 {
-	int	node, edge, start;
+	int	node_num, edge_num, start_node;
 	int fir_index, sec_index;
 	
-	cin >> node >> edge >> start;
-	//인접행렬 그래프 생성
-	while (edge)
+	cin >> node_num >> edge_num >> start_node;
+	//인접행렬 리스트 생성
+	for (int i = 1; i <= node_num; i++)
+	{
+		
+	}
+	while (edge_num)
 	{
 		cin >> fir_index >> sec_index;
-		graph[fir_index][sec_index] = 1;
-		graph[sec_index][fir_index] = 1;
-		edge--;
+		graph[fir_index];
+		edge_num--;
 	}
+	dfs(start_node);
+	cout << endl;
+	bfs();
+	return (0);
 }
