1fce01e72f106d3e024515c2ae6b513e8f19029f
KenWR <111475563+KenWR@users.noreply.github.com>
2023-11-08 08:59:30 +0900

    머지 정렬 완

--- 24060/24060.cpp
+++ 24060/24060.cpp
@@ -15,9 +15,10 @@ int main()
 	for (int i = 0; i < size_of_arr; i++)
 		std::cin >> arr[i];
 	merge_sort(0, size_of_arr - 1);
-	 for (int i = 0; i < size_of_arr; i++)
-	 	std::cout << arr[i] << ' ';
-	std::cout << num;
+	if (count < save_times)
+		std::cout << -1;
+	else
+		std::cout << num;
 	return (0);
 }
 
@@ -25,67 +26,50 @@ void	merge_sort(int p, int r)
 {
 	int	q;
 
-    if (p < r)
+	if (count == save_times)
+		return ;
+	if (p < r)
 	{
-        q = (p + r) / 2;
-        merge_sort(p, q);
-        merge_sort(q + 1, r);
-        merge_all(p, q, r);
-    }
+		q = (p + r) / 2;
+		merge_sort(p, q);
+		merge_sort(q + 1, r);
+		merge_all(p, q, r);
+	}
 }
 
 void	merge_all(int p, int q, int r) 
 {
 	int	i, j, t;
 
-    i = p; j = q + 1; t = 1;
-    while (i <= q and j <= r) 
+	i = p; j = q + 1; t = 1;
+	if (count == save_times)
+		return ;
+	while (i <= q and j <= r) 
 	{
-        if (arr[i] <= arr[j])
+		if (arr[i] <= arr[j])
 		{
-        	tmp[t++] = arr[i++]; // tmp[t] = arr[i]; t++; i++;
-			count++;
+			tmp[t++] = arr[i++];
 		}
-        else 
+		else 
 		{
-			tmp[t++] = arr[j++]; // tmp[t] = arr[j]; t++; j++;
-			count++;
-		}
-		if (count == save_times)
-		{
-			num = tmp[t - 1];
-			return ;
-		}
-    }
-    while (i <= q)  // 왼쪽 배열 부분이 남은 경우
-	{
-        tmp[t++] = arr[i++];
-		count++;
-		if (count == save_times)
-		{
-			num = tmp[t - 1];
-			return ;
+			tmp[t++] = arr[j++];
 		}
 	}
-    while (j <= r)  // 오른쪽 배열 부분이 남은 경우
+	while (i <= q)  // 왼쪽 배열 부분이 남은 경우
 	{
-        tmp[t++] = arr[j++];
-		count++;
-		if (count == save_times)
-		{
-			num = tmp[t - 1];
-			return ;
-		}
+		tmp[t++] = arr[i++];
 	}
-    i = p; t = 1;
-    while (i <= r)  // 결과를 A[p..r]에 저장
+	while (j <= r)  // 오른쪽 배열 부분이 남은 경우
 	{
-        arr[i++] = tmp[t++]; 
+		tmp[t++] = arr[j++];
+	}
+	i = p; t = 1;
+	while (i <= r)  // 결과를 A[p..r]에 저장
+	{
+		num = tmp[t];
+		arr[i++] = tmp[t++];
 		count++;
 		if (count == save_times)
-		{
-			num = tmp[t - 1];
 			return ;
-		}
 	}
 }
