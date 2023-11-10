e4c64e9120153f535a24b0b87cfea7b78bb41bba
Jiwon Jung <jiwojung@cx2r6s8.42seoul.kr>
2023-11-10 12:19:03 +0900

    역겨운 부동소수점 찾기해야됨

--- 1914/1914.cpp
+++ 1914/1914.cpp
@@ -1,47 +1,25 @@
 #include <iostream>
-#include <list>
 
-std::list<int>	a, b, c;
-
-void	hanoi(int n, std::list<int> from, std::list<int> temp, std::list<int> to)
-{
-	if (!from.empty())
-	{
-		to.push_front(from.front());
-		std::cout << from.front() << std::endl;
-		from.pop_front();
-	}
-	if (n == 1)
-		return ;
-	if (!temp.empty())
-	{
-		temp.push_front(to.front());
-		std::cout << from.front() << std::endl;
-		temp.pop_front();
-	}
-	hanoi(n - 1, from, to, temp);
-	hanoi(n - 1, temp, from, to);
+void hanoi(int n, int from, int temp, int to) {
+    if (n == 1) {
+        printf ("%d %d\n", from, to);
+        return;
+    }
+    hanoi(n - 1, from, to, temp);
+    printf ("%d %d\n", from, to);
+    hanoi(n - 1, temp, from, to);
 }
 
-int	main()
-{
-	int	n;
+int main() {
+    long n;
+    std::cin >> n;
 
-	std::cin >> n;
-	for (int i = 1; i <= n; i++)
-		a.push_back(i);
-	for (int i = 1; i <= n; i++)
-	{
-		std::cout << a.front() << ' ';
-		a.pop_front();
-	}
-	for (int i = 1; i <= n; i++)
-		a.push_back(i);
-	std::cout << std::endl;
-	hanoi(n, a, b, c);
-	// for (int i = 1; i <= n; i++)
-	// {
-	// 	std::cout << c.front() << ' ';
-	// 	c.pop_front();
-	// } 
+    if (n > 20) {
+        printf ((1 << n) - 1);
+    } else {
+        
+        hanoi(n, 1, 2, 3);
+    }
+
+    return 0;
 }
