0ab7703198dbcbdeca6a642238dd2c8f23352dfe
KenWR <skyqkqk1168@gmail.com>
2023-11-14 15:10:05 +0900

    시간초과 STL sort 로 해결

--- 1920/1920.cpp
+++ 1920/1920.cpp
@@ -1,73 +1,58 @@
-#include <iostream>
 #include <vector>
+#include <algorithm>
+#include <stdio.h>
 
 int	N, M, i;
-// std::vector<int> A;
-int	A[100005];
+std::vector<int>	A;
 
 int		partition(int start_idx, int end_idx);
 void	quick_sort(int start_idx, int end_idx);
-void	swap(int idx_1, int idx_2);
-int		binary_search(int n);
+bool	binary_search(int num);
 bool	check(int idx, int num);
 
 int main(){
 	int	num;
 
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(nullptr);
-	std::cout.tie(nullptr);
-
-	std::cin >> N;
+	scanf("%d", &N);
 	for (int i = 0; i < N; i++){
-		std::cin >> num;
-		// A.push_back(num);
-		A[i] = num;
+		scanf("%d", &num);
+		A.push_back(num);
 	}
-	quick_sort(0, N - 1);
-	std::cin >> M;
+	std::sort(A.begin(), A.end());
+	scanf("%d", &M);
 	for (int i = 0; i < M; i++){
-		std::cin >> num;
-		std::cout << binary_search(num);
-		std::cout << ' ';
+		scanf("%d", &num);
+		printf("%d ", binary_search(num));
 	}
 }
 
-void	quick_sort(int start_idx, int end_idx){
-	int	mid_idx;
+// void	quick_sort(int start_idx, int end_idx){
+// 	int	mid_idx;
 
-    if (start_idx < end_idx){
-        mid_idx = partition(start_idx, end_idx); 
-        quick_sort(start_idx, mid_idx - 1); 
-        quick_sort(mid_idx + 1, end_idx); 
-    }
-}
+//     if (start_idx < end_idx){
+//         mid_idx = partition(start_idx, end_idx); 
+//         quick_sort(start_idx, mid_idx - 1); 
+//         quick_sort(mid_idx + 1, end_idx); 
+//     }
+// }
 
-int	partition(int start_idx, int end_idx){
-	int	pivot;
+// int	partition(int start_idx, int end_idx){
+// 	int	pivot;
 
-    pivot = A[end_idx];       
-    for (int j = start_idx; j <= end_idx - 1; j++){
-		if (A[j] <= pivot) {
-			swap(start_idx, j);
-			start_idx++; 
-		}
-	}
-    if ((start_idx) != end_idx){
-		swap(start_idx, end_idx);
-	} 
-    return (start_idx);
-}
+//     pivot = A[end_idx];       
+//     for (int j = start_idx; j <= end_idx - 1; j++){
+// 		if (A[j] <= pivot) {
+// 			std::swap(A[start_idx], A[j]);
+// 			start_idx++; 
+// 		}
+// 	}
+//     if ((start_idx) != end_idx){
+// 		std::swap(A[start_idx], A[end_idx]);
+// 	} 
+//     return (start_idx);
+// }
 
-void	swap(int idx_1, int idx_2){
-	int	tmp;
-
-	tmp = A[idx_1];
-	A[idx_1] = A[idx_2];
-	A[idx_2] = tmp;
-}
-
-int    binary_search(int num){
+bool    binary_search(int num){
 	int	mid, left, right;
 
 	left = 0;
