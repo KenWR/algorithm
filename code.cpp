62a1d135954f59439c0808152ef882932317d9a6
KenWR <skyqkqk1168@gmail.com>
2023-11-19 23:32:25 +0900

    중량제한 컷

--- code.cpp
+++ code.cpp
@@ -1,57 +1,61 @@
+#include <array>
 #include <cstddef>
-#include <cstdlib>
 #include <iostream>
-#include <iterator>
 #include <utility>
 #include <vector>
 #include <algorithm>
 
-int M, N, L, lane, answer;
-std::pair<int, int> coord;
-std::vector<std::pair<int, int>> animals;
-std::vector<int> lanes;
+int N, M, A, B, C; // 섬(노드)의 개수, 다리(간선)의 개수, (A 섬1, B 섬2, C 중량)도로;
+std::vector<int> weights;
+std::array<bool, 10001> visit;
+std::vector<std::pair<int, int>> islands[10001];
+
 
 void    search();
+void	dfs(int weight, int island);
 
 int main() {
-	size_t  x, y;
+	std::pair<int, int> edge;
+
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(nullptr);
 	std::cout.tie(nullptr);
 
-	std::cin >> M >> N >> L;
+	std::cin >> N >> M;
 	for (int i = 0; i < M; i++) {
-		std::cin >> lane;
-		lanes.push_back(lane);
+		std::cin >> A >> B >> C;
+		islands[A].push_back(std::make_pair(B, C));
+		islands[B].push_back(std::make_pair(A, C));
+		weights.push_back(C);
 	}
-	for (int i = 0; i < N; i++) {
-		std::cin >> x >> y;
-		coord.first = x;
-		coord.second = y;
-		animals.push_back(coord);
-	}
-	std::sort(lanes.begin(), lanes.end());
+	std::cin >> A >> B;
+	std::sort(weights.begin(), weights.end());
 	search();
 }
 
 void    search() {
-	int	begin, mid, end;
+	int	begin = 0, mid, end = M - 1, answer = 0;
 
-	for (int i = 0; i < N; i++) {
-		begin = 0, end = M - 1;
-		while (begin <= end) {
-			mid = (begin + end) / 2;
-			if (abs(animals[i].first - lanes[mid]) + animals[i].second - L <= 0) {
-				answer++;
-				break ;
-			}
-			else if (animals[i].first > lanes[mid]) {
-				begin = mid + 1;
-			}
-			else {
-				end = mid - 1;
-			}
+	while (begin <= end) {
+		mid = (begin + end) / 2;
+		visit = {0, };
+		dfs(weights[mid], A);
+		if (visit[B] == true) {
+			answer = std::max(answer, weights[mid]);
+			begin = mid + 1;
+		}
+		else {
+			end = mid - 1;
 		}
 	}
 	std::cout << answer;
+}
+
+void	dfs(int weight, int island) {
+	visit[island] = true;
+	for (int i = 0; i < islands[island].size(); i++) {
+		if (visit[islands[island][i].first] == false && islands[island][i].second >= weight) {
+			dfs(weight, islands[island][i].first);
+		}
+	}
 }
