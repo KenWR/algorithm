e109bf7f678fbe514eb2eb87219005e32d35f406
KenWR <skyqkqk1168@gmail.com>
2023-12-09 15:48:15 +0900

    okay

--- code.cpp
+++ code.cpp
@@ -1,51 +1,92 @@
-#include <cmath>
-#include <cstdint>
-#include <cstring>
-#include <iostream>
 #include <algorithm>
+#include <ios>
+#include <iostream>
 #include <vector>
 
-int	T;
+int	N; 
+std::vector<int> possible_numbers;
 
-void	search(std::vector<int> folded_paper);
+void	fill_possible_num();
+void	say_num();
+void	compare_num(int	num, int strike, int ball);
+int		count_strike(int input_num, int comp_num);
+int		count_ball(int input_num, int comp_num);
 
 int	main() {
-	int index;
-
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(nullptr);
 	std::cout.tie(nullptr);
 
-	std::cin >> T;
-	for (int i = 0; i < T; i++) {
-		index = 0;
-		std::string	paper;
-		std::cin >> paper;
-		std::vector<int>	folded_paper;
-		while (paper[index]) {
-			folded_paper.push_back(paper[index] - '0');
-			index++;
-		}
-		search(folded_paper);
-	}
-	// 트리구조로
+	std::cin >> N;
+	fill_possible_num();
+	say_num();
+	std::cout << possible_numbers.size();
 }
 
-void	search(std::vector<int> folded_paper) {
-	int	low = 0, high = folded_paper.size() - 1;
+void	fill_possible_num() {
+	int	hundreds, tens, units;
 
-	while (low < high) {
-		if (folded_paper[low] == folded_paper[high]) {
-			std::cout << "NO" << '\n';
-			return ;
-		}
-		else if (low + 2 < high - 2 && ((folded_paper[low] == folded_paper[low + 1] && folded_paper[low] == folded_paper[low + 2]) \
-		|| (folded_paper[high] == folded_paper[high - 1] && folded_paper[high] == folded_paper[high - 2]))) {
-			std::cout << "NO" << '\n';
-			return ;
-		}
-		low++;
-		high--;
+	for (int i = 123; i < 999; i++) {
+		hundreds = (i / 100) % 10;
+		tens = (i / 10) % 10;
+		units = i % 10;
+		if (hundreds == tens || tens == units || units == hundreds \
+		|| tens == 0 || units == 0)
+			continue;
+		else
+			possible_numbers.push_back(i);
 	}
-	std::cout << "YES" << '\n';
+}
+
+void	say_num() {
+	for (int i = 0; i < N; i++) {
+		int	num, strike, ball;
+		std::cin >> num >> strike >> ball;
+		compare_num(num, strike, ball);
+	}
+}
+
+void	compare_num(int	num, int strike, int ball) {
+	int	count_stk = 0, count_bal = 0;
+
+	for (int i = 0; i < possible_numbers.size(); i++) {
+		count_stk = count_strike(num, possible_numbers[i]);
+		count_bal = count_ball(num, possible_numbers[i]);
+		if (count_stk != strike || count_bal != ball) {
+			possible_numbers.erase(possible_numbers.begin() + i);
+			i--;
+		}
+	}
+}
+
+int	count_strike(int input_num, int comp_num) {
+	int	num_of_strike = 0;
+	while (input_num) {
+		if (input_num % 10 == comp_num % 10) {
+			num_of_strike++;
+		}
+		input_num /= 10;
+		comp_num /= 10;
+	}
+	return num_of_strike;
+}
+
+int	count_ball(int input_num, int comp_num) {
+	int	num_of_ball = 0; 
+	int	hundreds = (comp_num / 100) % 10;
+	int tens = (comp_num / 10) % 10;
+	int units = comp_num % 10;
+
+	if (input_num % 10 == hundreds || input_num % 10 == tens) {
+		num_of_ball++;
+	}
+	input_num /= 10;
+	if (input_num % 10 == hundreds || input_num % 10 == units) {
+		num_of_ball++;
+	}
+	input_num /= 10;
+	if (input_num % 10 == tens || input_num % 10 == units) {
+		num_of_ball++;
+	}
+	return (num_of_ball);
 }
