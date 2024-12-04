fdd052609f8da468bada1380df65d119f49fb994
정지원 <111475563+KenWR@users.noreply.github.com>
2024-12-04 23:31:18 +0900

    feat: add solution and README for Check If a Word Occurs As a Prefix of Any Word in a Sentence

--- LeetCode/Easy/Check If a Word Occurs As a Prefix of Any Word in a Sentence/Solution.cpp
+++ LeetCode/Easy/Check If a Word Occurs As a Prefix of Any Word in a Sentence/Solution.cpp
@@ -1,3 +1,21 @@
-Input: sentence = "i love eating burger", searchWord = "burg"
-Output: 4
-Explanation: "burg" is prefix of "burger" which is the 4th word in the sentence.
+class Solution {
+public:
+    int isPrefixOfWord(string sentence, string searchWord) {
+        int index = (searchWord.front() != ' ');
+        for (int i = 0; i < sentence.size(); ++i) {
+            if (sentence[i] == ' ') {
+                while (i < sentence.size() && sentence[i] == ' ') i += 1;
+                index += 1;
+            }
+            if (sentence[i] == searchWord.front()) {
+                if (!sentence.compare(i, searchWord.size(), searchWord)) return index;
+            }
+            else {
+                while (i < sentence.size() && sentence[i] != ' ') i += 1;
+                i -= 1;
+            }
+        }
+
+        return -1;
+    }
+};
