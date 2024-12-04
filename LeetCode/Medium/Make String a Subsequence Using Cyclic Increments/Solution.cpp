5efbbc0cd1748c4bf60e80d75d891422dbadcf70
정지원 <111475563+KenWR@users.noreply.github.com>
2024-12-04 22:53:55 +0900

    feat: add solution and README for Make String a Subsequence Using Cyclic Increments

--- LeetCode/Medium/Make String a Subsequence Using Cyclic Increments/Solution.cpp
+++ LeetCode/Medium/Make String a Subsequence Using Cyclic Increments/Solution.cpp
@@ -1,5 +1,18 @@
-Input: str1 = "abc", str2 = "ad"
-Output: true
-Explanation: Select index 2 in str1.
-Increment str1[2] to become 'd'. 
-Hence, str1 becomes "abd" and str2 is now a subsequence. Therefore, true is returned.
+class Solution {
+public:
+    bool canMakeSubsequence(string str1, string str2) {
+        int len1 = str1.size(), len2 = str2.size();
+        if (len1 < len2) return false;
+
+        int idx2 = 0;
+        for (int idx1 = 0; idx1 < len1 && idx2 < len2; idx1++) {
+            if (str1[idx1] == str2[idx2] ||
+                str1[idx1] + 1 == str2[idx2] ||
+                str1[idx1] - 25 == str2[idx2]) {
+                idx2++;
+            }
+        }
+
+        return idx2 == len2;
+    }
+};
