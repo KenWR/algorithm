d8ce2eb4e20a841b1cec5c78a11f5bcc498e6b8c
정지원 <111475563+KenWR@users.noreply.github.com>
2024-12-04 23:29:07 +0900

    feat: add solution and README for Check If N and Its Double Exist

--- LeetCode/Easy/Check If N and Its Double Exist/Solution.cpp
+++ LeetCode/Easy/Check If N and Its Double Exist/Solution.cpp
@@ -1,3 +1,15 @@
-Input: arr = [10,2,5,3]
-Output: true
-Explanation: For i = 0 and j = 2, arr[i] == 10 == 2 * 5 == 2 * arr[j]
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        sort(arr.begin(), arr.end(), greater<>());
+
+        for (int i = 0; i < arr.size(); ++i) {
+            for (int j = i + 1; j < arr.size(); ++j) {
+                if (arr[i] < 0 && 2 * arr[i] == arr[j]) return true;
+                else if (arr[i] == 2 * arr[j]) return true;
+            }
+        }
+
+        return false;
+    }
+};
