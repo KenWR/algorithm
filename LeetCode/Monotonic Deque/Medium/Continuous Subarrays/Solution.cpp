09fe267e58d7628a85b4e3d209b09c1ebee7a5ce
정지원 <111475563+KenWR@users.noreply.github.com>
2024-12-14 21:57:39 +0900

    feat: add solution and README for Continuous Subarrays

---
+++ LeetCode/Monotonic Deque/Medium/Continuous Subarrays/Solution.cpp
@@ -0,0 +1,43 @@
+// Monotonic Deque
+// Not my solve!!
+// studying...
+
+class Solution {
+public:
+    long long continuousSubarrays(vector<int>& nums) {
+        deque<int> maxQ, minQ; // 현재 window에서의 최대, 최소 인덱스
+        long long count = 0;
+        int left = 0;
+
+        for (int i = 0; i < nums.size(); i++) { // 윈도우 크기 오른쪽으로 하나씩 늘려가며
+            // 점점 작아지는 최대값 목록 작성
+            while (!maxQ.empty() && nums[maxQ.back()] < nums[i]) {
+                maxQ.pop_back();
+            }
+            maxQ.push_back(i);
+
+            // 점점 커지는 최소값 목록 작성
+            while (!minQ.empty() && nums[minQ.back()] > nums[i]) {
+                minQ.pop_back();
+            }
+            minQ.push_back(i);
+
+            // 윈도우 축소 작업: 최대값과 최소값 차이가 2가 넘으면
+            while (!maxQ.empty() && !minQ.empty() && nums[maxQ.front()] - nums[minQ.front()] > 2) {
+                // 여기서 이제 최대, 최소값의 인덱스 차이를 통해 어느쪽에서 균형이 깨졌는지 알 수 있음
+                if (maxQ.front() < minQ.front()) {
+                    left = maxQ.front() + 1;
+                    maxQ.pop_front();
+                }
+                else {
+                    left = minQ.front() + 1;
+                    minQ.pop_front();
+                }
+            }
+            // 유효 서브 배열 개수를 추가
+            count += i - left + 1;
+        }
+
+        return count;
+    }
+};
