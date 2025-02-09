84fb36fda732b6e03457660d7524642bcdb73ee9
정지원 <111475563+KenWR@users.noreply.github.com>
2025-02-09 23:28:50 +0900

    feat: add solution and README for Count Number of Bad Pairs

--- LeetCode/Uncategorized/Medium/Count Number of Bad Pairs/Solution.cpp
+++ LeetCode/Uncategorized/Medium/Count Number of Bad Pairs/Solution.cpp
@@ -4,21 +4,20 @@ public:
         vector<int> gaps(nums.size(), 0);
         long long total_pairs = nums.size() * (nums.size() - 1) / 2;
         long long good_pairs = 0;
+        long long same_gap_count = 1;
 
         for (int i = 0; i < gaps.size(); ++i) {
             gaps[i] = nums[i] - i;
         }
-
         sort(gaps.begin(), gaps.end());
-        long long count = 1;
         for (int i = 1; i < gaps.size(); ++i) {
-            if (gaps[i - 1] == gaps[i]) count++;
+            if (gaps[i - 1] == gaps[i]) same_gap_count++;
             else {
-                good_pairs += count * (count - 1) / 2;
-                count = 1;
+                good_pairs += same_gap_count * (same_gap_count - 1) / 2;
+                same_gap_count = 1;
             }
         }
-        good_pairs += count * (count - 1) / 2;
+        good_pairs += same_gap_count * (same_gap_count - 1) / 2;
 
         return total_pairs - good_pairs;
     }
