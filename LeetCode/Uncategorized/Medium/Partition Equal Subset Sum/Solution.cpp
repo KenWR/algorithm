6454bb682b6ae856c4ad58dd82f20d5b920c918f
정지원 <111475563+KenWR@users.noreply.github.com>
2025-04-07 23:37:58 +0900

    feat: add solution and README for Partition Equal Subset Sum

--- LeetCode/Uncategorized/Medium/Partition Equal Subset Sum/Solution.cpp
+++ LeetCode/Uncategorized/Medium/Partition Equal Subset Sum/Solution.cpp
@@ -1,23 +1,3 @@
-class Solution {
-public:
-    bool canPartition(vector<int>& nums) {
-        int sum = 0;
-        
-        for (const auto &num : nums) {
-            sum += num;
-        }
-
-        if (sum % 2 == 1) {
-            return false;
-        }
-
-        bitset<10001> bit(1);
-        for (auto i : nums) {
-            bit |= bit << i;
-            if (bit[sum / 2])
-                return true;
-        }
-
-        return bit[sum / 2];
-    }
-};
+Input: nums = [1,5,11,5]
+Output: true
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
